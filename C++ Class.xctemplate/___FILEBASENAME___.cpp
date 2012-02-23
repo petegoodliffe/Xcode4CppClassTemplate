66e60ff7ec6356d8fc50689429c4faa84616f3de
Pete Goodliffe <pgoodliffe@numark.com>
2012-02-23 10:49:31 +0000

    Initial checkin of my template

---
+++ C++ Class.xctemplate/___FILEBASENAME___.cpp
@@ -0,0 +1,10 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#include "___FILEBASENAME___.h"
+
