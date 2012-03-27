49684d823134bb443e607c0d12169affcb670233
Pete Goodliffe <pgoodliffe@numark.com>
2012-03-27 16:22:44 +0100

    Updated for Xcode 4.3.2.

---
+++ install-Xcode4.3.2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+###############################################################################
+# Xcode C++ template install script
+# Pete Goodliffe
+# (c) 2012 Pete Goodliffe, All rights reserved.
+###############################################################################
+
+export XCODE_VERSION=4.3.2
+
+./install-Xcode4.3.sh
+
+
